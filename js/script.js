60a04d4c89fe80945ef92ff52b876e7c073cc940
Viktor Tsolka <bobick.zdoh@gmail.com>
2020-11-16 14:45:48 +0200

    добавляет ленивую загрузку

--- js/script.js
+++ js/script.js
@@ -55,57 +55,80 @@ function onPressArrowLeftAndArrowRight(event) {
   if (event.code === 'ArrowRight') {
     index = currentIndexImg += 1;
   }
-  if (index < 0 || index > 8) return;
+  if (index < 0 || index > galleryItems.length - 1) return;
   imgModalRef.src = galleryItems[index].original;
   imgModalRef.alt = galleryItems[index].description;
 }
 
-const getTemplate = ({ preview, original, description }, i) => {
-  const li = document.createElement('li');
-  li.classList.add('gallery__item');
+// const getTemplate = ({ preview, original, description }, i) => {
+//   const li = document.createElement('li');
+//   li.classList.add('gallery__item');
 
-  const a = document.createElement('a');
-  a.classList.add('gallery__link');
-  a.setAttribute('href', original);
+//   const a = document.createElement('a');
+//   a.classList.add('gallery__link');
+//   a.setAttribute('href', original);
 
-  const img = document.createElement('img');
-  img.classList.add('gallery__image');
-  img.setAttribute('src', preview);
-  img.dataset.source = original;
-  img.setAttribute('alt', description);
-  img.dataset.index = i;
+//   const img = document.createElement('img');
+//   img.classList.add('gallery__image');
+//   img.setAttribute('src', preview);
+//   img.dataset.source = original;
+//   img.setAttribute('alt', description);
+//   img.dataset.index = i;
 
-  a.appendChild(img);
-  li.appendChild(a);
-  return li;
-};
-
-const arrElements = galleryItems.map((object, index) =>
-  getTemplate(object, index),
-);
-galleryRef.append(...arrElements);
-
-// const getTemplate = array => {
-//   return array
-//     .map(
-//       (object, i) =>
-//         `<li class="gallery__item">
-//       <a
-//         class="gallery__link"
-//         href="${object.original}"
-//       >
-//         <img
-//           class="gallery__image"
-//           src="${object.preview}"
-//           data-index="${i}"
-//           data-source="${object.original}"
-//           alt="${object.description}"
-//         />
-//       </a>
-//     </li>`,
-//     )
-//     .join('');
+//   a.appendChild(img);
+//   li.appendChild(a);
+//   return li;
 // };
 
-// const template = getTemplate(galleryItems);
-// const innerLiToElement = galleryRef.insertAdjacentHTML('beforeend', template);
+// const arrElements = galleryItems.map((object, index) =>
+//   getTemplate(object, index),
+// );
+// galleryRef.append(...arrElements);
+
+// ===============================================
+// ленивая загрузка
+const getTemplate = array => {
+  return array
+    .map(
+      (object, i) =>
+        `<li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${object.original}"
+      >
+        <img
+          class="gallery__image"
+          src=""
+          data-lazy="${object.preview}"
+          data-index="${i}"
+          data-source="${object.original}"
+          alt="${object.description}"
+        />
+      </a>
+    </li>`,
+    )
+    .join('');
+};
+
+const template = getTemplate(galleryItems);
+const innerLiToElement = galleryRef.insertAdjacentHTML('beforeend', template);
+
+const images = document.querySelectorAll('img[data-lazy]');
+
+const onEntry = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.src = entry.target.dataset.lazy;
+
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const options = {
+  rootMargin: '20px',
+};
+
+const io = new IntersectionObserver(onEntry, options);
+
+images.forEach(image => io.observe(image));
